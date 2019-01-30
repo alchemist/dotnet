9c813610839a6a5b14b34500853b4ca205fef072
LP (BB) <lee.prosser@embersoft.co.uk>
2019-01-30 09:15:21 +0000

    halfway

--- src/generators/codegen-proxy.ts
+++ src/generators/codegen-proxy.ts
@@ -1,14 +1,27 @@
-/*
+console.log("LOADING ELECTRON");
 import {remote} from "electron";
+const fs = remote.require('fs');
+const app = remote.app;
+console.log("LOADED ELECTRON");
+
 import path from "path";
 
-const assemblyDir = `${__dirname}/codegen/CodeGen.exe`;
+console.log("LOADING EDGE");
+const assemblyDir = `${app.getAppPath()}/codegen/CodeGen.exe`;
+
+if (!fs.existsSync(assemblyDir)) {
+    console.log("Unable to find CodeGen assembly, this is required for dotnet plugin to operate");
+    throw "Unable to find CodeGen assembly, this is required for dotnet plugin to operate";
+}
+
 eval(`process.env.EDGE_USE_CORECLR = 1;`);
-eval(`process.env.EDGE_APP_ROOT = "${assemblyDir}";`);
+//eval(`process.env.EDGE_APP_ROOT = "${assemblyDir}";`);
 
 const {func} = remote.require("electron-edge-js");
+console.log("LOADED EDGE");
 
 const funcP = (codeOrDescriptor: any) => {
+    console.log("STARTING FUNC");
     const handle = func(codeOrDescriptor);
     return (data?: any) : Promise<any> => {
         return new Promise((resolve, reject) => {
@@ -30,10 +43,4 @@ export class CodegenProxy
         typeName: 'CodeGen.Formatters.GeneralCodeFormatter',
         methodName: 'FormatCode'
     });
-}
-*/
-
-export class CodegenProxy
-{
-    public static formatCode = (foo) => foo;
 }
