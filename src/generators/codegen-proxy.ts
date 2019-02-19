2febed08387d97b5d3926d5f5009209fc52de105
LP (BB) <lee.prosser@embersoft.co.uk>
2019-02-19 15:55:53 +0000

    An attempt at getting webpack running properly
    Removes the vue cli stuff and also tries to get edge running, this is not a stable version.

--- src/generators/codegen-proxy.ts
+++ src/generators/codegen-proxy.ts
@@ -1,12 +1,11 @@
 console.log("LOADING ELECTRON");
 import {remote} from "electron";
+import {join} from "path";
 const fs = remote.require('fs');
 const app = remote.app;
 console.log("LOADED ELECTRON");
 
-import path from "path";
-
-console.log("LOADING EDGE");
+console.log("CALCULATING EDGE DIRS");
 const assemblyDir = `${app.getAppPath()}/codegen/CodeGen.exe`;
 
 if (!fs.existsSync(assemblyDir)) {
@@ -14,10 +13,11 @@ if (!fs.existsSync(assemblyDir)) {
     throw "Unable to find CodeGen assembly, this is required for dotnet plugin to operate";
 }
 
-eval(`process.env.EDGE_USE_CORECLR = 1;`);
-//eval(`process.env.EDGE_APP_ROOT = "${assemblyDir}";`);
+process.env.EDGE_USE_CORECLR = <any>1;
+process.env.EDGE_APP_ROOT = assemblyDir;
 
-const {func} = remote.require("electron-edge-js");
+console.log("LOADING EDGE");
+import {func} from "electron-edge-js";
 console.log("LOADED EDGE");
 
 const funcP = (codeOrDescriptor: any) => {
@@ -39,7 +39,7 @@ const funcP = (codeOrDescriptor: any) => {
 export class CodegenProxy
 {
     public static formatCode = funcP({
-        assemblyFile: path.join(assemblyDir, 'CodeGen.dll'),
+        assemblyFile: join(assemblyDir, 'CodeGen.dll'),
         typeName: 'CodeGen.Formatters.GeneralCodeFormatter',
         methodName: 'FormatCode'
     });
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode: 'production',
+    target: "electron-renderer",
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'alchemist.dotnet.common.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        'scss': 'vue-style-loader!css-loader!sass-loader'
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    externals: [
+        "@treacherous/core",
+        "@treacherous/view",
+        "@treacherous/vue",
+        "@treacherous/decorators",
+        "@alchemist/core",
+        "uuid",
+        "tslib",
+        "vue",
+        "vue-class-component",
+        "vue-property-decorator",
+        "vuex",
+        "vuex-class",
+        "electron",
+        "electron-edge-js"
+    ],
+    optimization: {
+        minimize: false
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
